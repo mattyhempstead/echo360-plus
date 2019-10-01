7148ad29613d53893378b5821764a5173a6b6aab
Matty <matty.hempstead@gmail.com>
2019-10-01 15:18:11 +1000

    Add day to lecture dates

---
+++ src/scripts/day-with-dates.js
@@ -0,0 +1,16 @@
+/// This script essentially needs to create a chain of triggers to get the mutation code to execute at the right time.
+console.log('Running day with dates script')
+
+// Runs whenever a new element is inserted into DOM containing lectures
+// This adds a trigger to every lecture which will update the current lecture info
+// whenever a particular lecture is selected.
+document.getElementsByClassName('contents-wrapper')[0].addEventListener('DOMNodeInserted', (evt) => {
+
+  // Only add trigger if element is a lecture
+  if (evt.target.className && evt.target.className.indexOf('class-row') != -1) {
+    const dateElement = evt.target.getElementsByClassName('date')[0]
+    const day = new Date(dateElement.innerHTML).toString().substr(0,3)
+    dateElement.innerHTML = '<span style="color: grey">' + day + '</span>, ' + dateElement.innerHTML
+  } 
+  
+})
