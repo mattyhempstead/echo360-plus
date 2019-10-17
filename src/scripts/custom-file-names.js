058fb4fc875eab9cfa113f0be66e4ec37a75884d
Matty <matty.hempstead@gmail.com>
2019-10-18 01:20:53 +1100

    Fix scripts not executing in time to trigger listeners
    Resolves #3

--- src/scripts/custom-file-names.js
+++ src/scripts/custom-file-names.js
@@ -1,24 +1,48 @@
-/// This script essentially needs to create a chain of triggers to get 
-/// the mutation code to execute at the right time.
-console.log('Running custom file name script')
+// This script adds custom file names to downloaded lectures
 
-// Information about the selected lecture
-let lecInfo = {}
+(() => {
+  /// This script essentially needs to create a chain of triggers to get 
+  /// the mutation code to execute at the right time.
+  console.log('Echo360+ - custom file names')
+
+  // Information about the selected lecture
+  let lecInfo = {}
+
+  // The div which hold all the lectures
+  const lectureContainer = document.getElementsByClassName('contents-wrapper')[0]
+
+  // Sometimes lecture rows load before the script is executed
+  // This can occur when the page is opened in a new tab yet the user does not have the tab open
+  // Thus we should begin the script by adding listeners to all existing lecture elements
+  for (rowElement of lectureContainer.getElementsByClassName('class-row')) {
+    // Only execute for lectures with class name exactly 'class-row'
+    // This will ensure the modification is only applied to uploaded lectures
+    if (rowElement.className === 'class-row') addListenerToLectureRow(rowElement)
+  }
 
 
-// Runs whenever a new element is inserted into DOM containing lectures
-// This adds a trigger to every lecture which will update the current lecture info
-// whenever a particular lecture is selected.
-const contentElement = document.getElementsByClassName('contents-wrapper')[0]
-contentElement.addEventListener('DOMNodeInserted', (evt) => {
+  // Runs whenever a new element is inserted into DOM containing lectures
+  // This adds a trigger to every lecture which will update the current lecture info
+  // whenever a particular lecture is selected.
+  lectureContainer.addEventListener('DOMNodeInserted', (evt) => {
+    // Only add trigger if element is a lecture which has also finished uploading
+    if (evt.target.className === 'class-row') {
+      addListenerToLectureRow(evt.target)
+    } 
+  })
 
-  // Only add trigger if element is a lecture which has also finished uploading
-  if (evt.target.className == 'class-row') {
-    const openMenuElement = evt.target.getElementsByClassName('menu-opener')[0]
+
+  /**
+   * Adds a click listener to a given lecture row element
+   * This allows the script to store information on whichever lecture the user has selected
+   * @param {HTMLElement} rowElement - The HTML element representing a row in the lecture list
+   */
+  function addListenerToLectureRow(rowElement) {
+    const openMenuElement = rowElement.getElementsByClassName('menu-opener')[0]
     openMenuElement.addEventListener('click', () => {
 
       // Store info about the particular lecture which has been clicked
-      let lectureString = evt.target.getAttribute('aria-label')
+      let lectureString = rowElement.getAttribute('aria-label')
 
       // Get unit of study (e.g. DATA1002)
       lecInfo.uos = lectureString.split('(')[1].split(' ')[0]
@@ -32,59 +56,42 @@ contentElement.addEventListener('DOMNodeInserted', (evt) => {
       lecInfo.date = dateString
 
     })
-  } 
-  
-  // // Element is the pop menu which appears with a "Download Original" button
-  // else if (evt.target.className == 'menu-items') {
-  //   const downloadOriginalElement = evt.target.getElementsByTagName('li')[1]
-  //   downloadOriginalElement.addEventListener('click', () => {
-  //     console.log("downloading orignals")
-
-  //     const modals = document.body.getElementsByClassName('modal ')
-  //     for (modal of modals) {
-  //       console.log(modal)
-  //     }
-  //     console.log(modals, modals.length, modals[2])
-  //     x.push(modals)
-  //   })
-
-
-  // }
-
-})
-
-
-// A listener for when a model is opened
-// This will change the link for the "Download" button
-let observer
-document.body.addEventListener('DOMNodeInserted', (evt) => {
-  // Update Download button DOM when a modal is added
-  if (evt.target.className == 'modal ') {
-    // Remove old observer when a new modal is opened
-    if (observer) observer.disconnect()
-
-    const downloadBtnElement = document.getElementsByClassName('downloadBtn')[0]
-
-    observer = new MutationObserver((mutations) => {
-      for (mutation of mutations) {
-        // Only apply change if not already updated
-        if (!downloadBtnElement.getAttribute('href').endsWith('custom')) {
-          updateBtnHref(downloadBtnElement)
-        }
-      }
-    });
-
-    observer.observe(downloadBtnElement, { attributes: true })
   }
-})
 
 
-// Updates the href of the download button to have custom file name
-function updateBtnHref(btn) {
-  // Modify download link to give custom name to file
-  let fileExtension = btn.getAttribute('href').split('.').pop()
-  let customFileName = `${lecInfo.uos}-${lecInfo.date.getFullYear()}-${lecInfo.date.getMonth()+1}-${lecInfo.date.getDate()}.${fileExtension}`
-  let url = new URL(btn.href)
-  url.searchParams.set('fileName', customFileName)
-  btn.setAttribute('href', url.toString() + '&custom')
-}
+  // A listener for when a model is opened
+  // This will change the link for the "Download" button
+  let observer
+  document.body.addEventListener('DOMNodeInserted', (evt) => {
+    // Update Download button DOM when a modal is added
+    if (evt.target.className == 'modal ') {
+      // Remove old observer when a new modal is opened
+      if (observer) observer.disconnect()
+
+      const downloadBtnElement = document.getElementsByClassName('downloadBtn')[0]
+
+      observer = new MutationObserver((mutations) => {
+        for (mutation of mutations) {
+          // Only apply change if not already updated
+          if (!downloadBtnElement.getAttribute('href').endsWith('custom')) {
+            updateBtnHref(downloadBtnElement)
+          }
+        }
+      });
+
+      observer.observe(downloadBtnElement, { attributes: true })
+    }
+  })
+
+
+  // Updates the href of the download button to have custom file name
+  function updateBtnHref(btn) {
+    // Modify download link to give custom name to file
+    let fileExtension = btn.getAttribute('href').split('.').pop()
+    let customFileName = `${lecInfo.uos}-${lecInfo.date.getFullYear()}-${lecInfo.date.getMonth()+1}-${lecInfo.date.getDate()}.${fileExtension}`
+    let url = new URL(btn.href)
+    url.searchParams.set('fileName', customFileName)
+    btn.setAttribute('href', url.toString() + '&custom')
+  }
+
+})()
--- src/scripts/day-with-dates.js
+++ src/scripts/day-with-dates.js
@@ -1,20 +1,39 @@
-/// This script essentially needs to create a chain of triggers to get the mutation code to execute at the right time.
-console.log('Running day with dates script')
+// This script adds day strings to each lecture on the list of lectures (e.g. Fri)
 
-// Runs whenever a new element is inserted into DOM containing lectures
-// This adds a trigger to every lecture which will update the current lecture info
-// whenever a particular lecture is selected.
-document.getElementsByClassName('contents-wrapper')[0].addEventListener('DOMNodeInserted', (evt) => {
+(() => {
+  console.log('Echo360+ - day with dates')
 
-  // Only add trigger if element is a lecture
-  if (evt.target.className && evt.target.className.indexOf('class-row') !== -1) {
-    const dateElement = evt.target.getElementsByClassName('date')[0]
+  // The div which hold all the lectures
+  const lectureContainer = document.getElementsByClassName('contents-wrapper')[0]
+
+  // Sometimes lecture rows load before the script is executed
+  // This can occur when the page is opened in a new tab yet the user does not have the tab open
+  // Thus we should begin the script by adding days to all existing lecture elements
+  for (rowElement of lectureContainer.getElementsByClassName('class-row')) {
+    addDayStringToRow(rowElement)
+  }
+
+  // As lectures are loaded asyncronously after page loads, we need a listener to check
+  // whenever a new element is inserted into the DOM element which contains the lectures.
+  lectureContainer.addEventListener('DOMNodeInserted', (evt) => {
+    // Only add trigger if element is a lecture
+    if (evt.target.className && evt.target.className.indexOf('class-row') !== -1) {
+      addDayStringToRow(evt.target)
+    } 
+  })
+
+  /**
+   * Adds a date string to a given lecture row element
+   * @param {HTMLElement} rowElement - The HTML element representing a row in the lecture list
+   */
+  function addDayStringToRow(rowElement) {
+    const dateElement = rowElement.getElementsByClassName('date')[0]
 
     // Only add a day if it has not been added already
     if (dateElement.getElementsByClassName("day").length === 0) {
       const day = new Date(dateElement.innerHTML).toString().substr(0,3)
       dateElement.innerHTML = `<span class="day" style="color: grey">${day}</span>, ${dateElement.innerHTML}`  
     }
-  } 
-  
-})
+  }
+
+})()
