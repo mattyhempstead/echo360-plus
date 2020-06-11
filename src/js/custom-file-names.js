9d2497bd99af82ad063cd0fafbe52dd4109a4cda
Matty <matty.hempstead@gmail.com>
2020-06-11 17:41:23 +1000

    Fix custom-file-names script

--- src/js/custom-file-names.js
+++ src/js/custom-file-names.js
@@ -1,35 +1,28 @@
 // This script adds custom file names to downloaded lectures
 
 (() => {
-  /// This script essentially needs to create a chain of triggers to get 
-  /// the mutation code to execute at the right time.
-  console.log('Echo360+ - custom file names')
+  console.log('Echo360+ - custom file names');
 
   // Information about the selected lecture
-  let lecInfo = {}
-
-  // The div which hold all the lectures
-  const lectureContainer = document.getElementsByClassName('contents-wrapper')[0]
-
-  // Sometimes lecture rows load before the script is executed
-  // This can occur when the page is opened in a new tab yet the user does not have the tab open
-  // Thus we should begin the script by adding listeners to all existing lecture elements
-  for (rowElement of lectureContainer.getElementsByClassName('class-row')) {
-    // Only execute for lectures with class name exactly 'class-row'
-    // This will ensure the modification is only applied to uploaded lectures
-    if (rowElement.className === 'class-row') addListenerToLectureRow(rowElement)
-  }
+  let lecInfo = {};
+  lecInfo.uos = document.querySelector('.course-section-header h1').innerText.split(" ")[0];
 
 
-  // Runs whenever a new element is inserted into DOM containing lectures
-  // This adds a trigger to every lecture which will update the current lecture info
-  // whenever a particular lecture is selected.
-  lectureContainer.addEventListener('DOMNodeInserted', (evt) => {
-    // Only add trigger if element is a lecture which has also finished uploading
-    if (evt.target.className === 'class-row') {
-      addListenerToLectureRow(evt.target)
-    } 
-  })
+  // Listen for any new elements to DOM and filter for "class-row"
+  new MutationObserver((mutationsList, obsvr) => {
+    for (let mutation of mutationsList) {
+      for (let node of mutation.addedNodes) {
+        // "class-row" elements
+        if (node.className == 'class-row')
+          addListenerToLectureRow(node);
+      
+        // "class-row" elements in groups
+        if (node.getElementsByClassName)
+          Array.from(node.getElementsByClassName('class-row'))
+            .map(addListenerToLectureRow);
+      }
+    }
+  }).observe(document.body, {childList:true, subtree:true});
 
 
   /**
@@ -38,23 +31,17 @@
    * @param {HTMLElement} rowElement - The HTML element representing a row in the lecture list
    */
   function addListenerToLectureRow(rowElement) {
-    const openMenuElement = rowElement.getElementsByClassName('menu-opener')[0]
+    const openMenuElement = rowElement.getElementsByClassName('menu-opener')[0];
+    if (!openMenuElement) return;
+
+    // Clicking on lecture will store current date
     openMenuElement.addEventListener('click', () => {
-
-      // Store info about the particular lecture which has been clicked
-      let lectureString = rowElement.getAttribute('aria-label')
-
-      // Get unit of study (e.g. DATA1002)
-      lecInfo.uos = lectureString.split('(').slice(-1)[0].split(' ')[0]
-
-      // Decode date (not time) into javascript Date object
-      let dateString = lectureString.split('-')[0]
-      dateString = dateString.split(' ')
-      dateString.pop()
-      dateString = dateString.join(' ')
-      dateString = new Date(dateString)
-      lecInfo.date = dateString
-
+      const dateElement = rowElement.querySelector('span.date');
+      if (!dateElement) {
+        lecInfo.date = new Date(0);   // Use 0000-00-00 if no date exists
+      } else {
+        lecInfo.date = new Date(dateElement.innerText);
+      }
     })
   }
 
@@ -66,15 +53,15 @@
     // Update Download button DOM when a modal is added
     if (evt.target.className == 'modal ') {
       // Remove old observer when a new modal is opened
-      if (observer) observer.disconnect()
+      if (observer) observer.disconnect();
 
-      const downloadBtnElement = document.getElementsByClassName('downloadBtn')[0]
+      const downloadBtnElement = document.getElementsByClassName('downloadBtn')[0];
 
       observer = new MutationObserver((mutations) => {
         for (mutation of mutations) {
           // Only apply change if not already updated
           if (!downloadBtnElement.getAttribute('href').endsWith('custom')) {
-            updateBtnHref(downloadBtnElement)
+            updateBtnHref(downloadBtnElement);
           }
         }
       });
@@ -87,15 +74,18 @@
   // Updates the href of the download button to have custom file name
   function updateBtnHref(btn) {
     // Modify download link to give custom name to file
-    let fileExtension = btn.getAttribute('href').split('.').pop()
+    let fileExtension = btn.getAttribute('href').split('.').pop();
 
-    const year = lecInfo.date.getFullYear();
-    const month = ((lecInfo.date.getMonth()+1)+'').padStart(2, '0');
-    const day = (lecInfo.date.getDate()+'').padStart(2, '0');
-    let customFileName = `${lecInfo.uos}-${year}-${month}-${day}.${fileExtension}`
-    let url = new URL(btn.href)
-    url.searchParams.set('fileName', customFileName)
-    btn.setAttribute('href', url.toString() + '&custom')
+    const isValidDate = lecInfo.date.getTime() != 0;
+    const year = isValidDate ? lecInfo.date.getFullYear() : '0000';
+    const month = isValidDate ? ((lecInfo.date.getMonth()+1)+'').padStart(2, '0') : '00';
+    const day = isValidDate ? (lecInfo.date.getDate()+'').padStart(2, '0') : '00';
+
+    let customFileName = `${lecInfo.uos}-${year}-${month}-${day}.${fileExtension}`;
+
+    let url = new URL(btn.href);
+    url.searchParams.set('fileName', customFileName);
+    btn.setAttribute('href', url.toString() + '&custom');
   }
 
 })()
